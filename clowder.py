77fa5194a0a7bf8a2d33a1aa98233ee070ec1edd
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-06-22 11:37:58 -0700

    Full Suite Of Tests

--- clowder.py
+++ clowder.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 import datetime
+
 from requests_futures.sessions import FuturesSession
 
+
+# The URL of the Clowder API
+CLOWDER_API_URL = 'http://www.clowder.io/api'
+# Allowed keys for data given
 ALLOWED_KEYS = ('url', 'value', 'status', 'frequency')
 
 api_key = None
 
-def _send(data):
 
-    url = data.get('url', 'http://www.clowder.io/api')
+def _send(data):
+    """Send data to the Clowder API.
+
+    :param data: Dictionary of API data
+    :type data: dict
+    """
+    url = data.get('url', CLOWDER_API_URL)
 
     session = FuturesSession()
 
@@ -24,8 +34,13 @@ def _send(data):
 
     session.post(url, data=data)
 
-def ok(data):
 
+def ok(data):
+    """Send a success signal to clowder.
+
+    :param data: Data to be sent along (Should not include 'status')
+    :type data: dict
+    """
     if 'status' in data:
         raise AttributeError('Status should not be provided to okay')
     else:
@@ -33,19 +48,32 @@ def ok(data):
 
     _send(data)
 
-def fail(data):
 
+def fail(data):
+    """Send a failure signal to clowder.
+
+    :param data: Data to be sent along (Should not include 'status')
+    :type data: dict
+    """
     if 'status' in data:
-        raise AttributeError('Status should not be provided to okay')
+        raise AttributeError('Status should not be provided to fail')
     else:
         data['status'] = -1
 
     _send(data)
 
+
 def _clean_frequency(frequency):
+    """Converts a frequency value to an integer. Raises an error if an invalid
+    type is given.
+
+    :param frequency: A frequency
+    :type frequency: int or datetime.timedelta
+    :rtype: int
+    """
     if isinstance(frequency, int):
         return frequency
     elif isinstance(frequency, datetime.timedelta):
         return int(frequency.total_seconds())
 
-    raise ValueError('Invalid frequency')
+    raise ValueError('Invalid frequency {!r}'.format(frequency))
--- tests.py
+++ tests.py
@@ -1,14 +1,102 @@
-import clowder
+# -*- coding: utf-8 -*-
+import contextlib
 import datetime
-import psutil
+import unittest
 
-clowder.ok({
-   'name': 'CPU Percent',
-   'value': psutil.cpu_percent(interval=1),
-   'frequency': datetime.timedelta(minutes=0.5)
-})
+import clowder
+import mock
 
-clowder.ok({
-   'name': 'Memory Utilization',
-   'value': psutil.phymem_usage().percent
-})
+# import psutil
+
+
+class BaseClowderTestCase(unittest.TestCase):
+    """Base class for all clowder test cases."""
+
+    def assert_send_contains_data(self, send_mock, key, value):
+        """Assert that the given send mock was called with the given key and
+        value pair.
+
+        :param key: A key
+        :type key: hashable
+        :param value: The expected value
+        :type value: mixed
+        """
+        self.assertIn(key, send_mock.call_args[0][0])
+        self.assertEqual(value, send_mock.call_args[0][0][key])
+
+
+class TestCleanFrequency(unittest.TestCase):
+
+    def test_should_return_value_if_int_given(self):
+        self.assertEqual(100, clowder._clean_frequency(100))
+
+    def test_should_return_total_seconds_if_timedelta_given(self):
+        fixture = datetime.timedelta(hours=1)
+        self.assertEqual(
+            fixture.total_seconds(), clowder._clean_frequency(fixture)
+        )
+
+    def test_should_raise_error_if_any_other_type_value_given(self):
+        self.assertRaisesRegexp(
+            ValueError,
+            "Invalid frequency 'hello'",
+            clowder._clean_frequency,
+            "hello"
+        )
+
+
+class TestFail(BaseClowderTestCase):
+
+    def test_should_raise_error_if_status_given(self):
+        self.assertRaisesRegexp(
+            AttributeError,
+            "Status should not be provided to fail",
+            clowder.fail,
+            {'status': 'should fail'}
+        )
+
+    @mock.patch('clowder._send')
+    def test_should_send_value_provided_along(self, send):
+        clowder.fail({'value': 'Invalid stuff'})
+        send.assert_called_once()
+        self.assert_send_contains_data(send, 'value', 'Invalid stuff')
+
+    @mock.patch('clowder._send')
+    def test_should_send_status_of_negative_one(self, send):
+        clowder.fail({'value': "Invalid stuff"})
+        send.assert_called_once()
+        self.assert_send_contains_data(send, 'status', -1)
+
+
+class TestOk(BaseClowderTestCase):
+
+    def test_should_raise_error_if_status_given(self):
+        self.assertRaisesRegexp(
+            AttributeError,
+            "Status should not be provided to ok",
+            clowder.ok,
+            {'status': 'should fail'}
+        )
+
+    @mock.patch('clowder._send')
+    def test_should_send_value_provided_along(self, send):
+        clowder.ok({'value': 'Invalid stuff'})
+        send.assert_called_once()
+        self.assert_send_contains_data(send, 'value', 'Invalid stuff')
+
+    @mock.patch('clowder._send')
+    def test_should_send_status_of_one(self, send):
+        clowder.ok({'value': "Invalid stuff"})
+        send.assert_called_once()
+        self.assert_send_contains_data(send, 'status', 1)
+
+# clowder.ok({
+#    'name': 'CPU Percent',
+#    'value': psutil.cpu_percent(interval=1),
+#    'frequency': datetime.timedelta(minutes=0.5)
+# })
+
+# clowder.ok({
+#    'name': 'Memory Utilization',
+#    'value': psutil.phymem_usage().percent
+# })
